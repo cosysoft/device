3f1de43146c13111cd8f8f162b58f7e1fb7c097d
Bluesky Yao <remotejavasky@163.com>
2015-04-04 22:26:20 +0800

    from maven to gradle

---
+++ device-node/src/main/java/SampleController.java
@@ -0,0 +1,23 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by 兰天 on 2015/4/2.
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
