4d2edbfbf0491d06877cf04611f4fc4e9b234555
ylt姚兰天 <ltyao@Ctrip.com>
2016-02-20 14:49:25 +0800

    add android sdk version

--- device-api/src/main/java/com/github/cosysoft/device/android/DeviceTargetPlatform.java
+++ device-api/src/main/java/com/github/cosysoft/device/android/DeviceTargetPlatform.java
@@ -14,60 +14,61 @@
 package com.github.cosysoft.device.android;
 
 public enum DeviceTargetPlatform {
-	ANDROID10("2.3.3"), ANDROID11("3.0"), ANDROID12("3.1"), ANDROID13("3.2"), ANDROID14(
-			"4.0"), ANDROID15("4.0.3"), ANDROID16("4.1.2"), ANDROID17("4.2.2"), ANDROID18(
-			"4.3"), ANDROID19("4.4"), ANDROID20("4.4W"), ANDROID21("5.0");
-	public static final String ANDROID = "ANDROID";
+  ANDROID10("2.3.3"), ANDROID11("3.0"), ANDROID12("3.1"), ANDROID13("3.2"), ANDROID14(
+      "4.0"), ANDROID15("4.0.3"), ANDROID16("4.1.2"), ANDROID17("4.2.2"), ANDROID18(
+      "4.3"), ANDROID19("4.4"), ANDROID20("4.4W"), ANDROID21("5.0"), ANDROID22("5.1"),
+  ANDROID23("6.0");
+  public static final String ANDROID = "ANDROID";
 
-	private String versionNumber;
-	private String api;
+  private String versionNumber;
+  private String api;
 
-	DeviceTargetPlatform(String version) {
-		this.versionNumber = version;
-		this.api = this.name().replace(ANDROID, "");
-	}
+  DeviceTargetPlatform(String version) {
+    this.versionNumber = version;
+    this.api = this.name().replace(ANDROID, "");
+  }
 
-	public String getSdkFolderName() {
-		return name().replace(ANDROID, "android-");
-	}
+  public String getSdkFolderName() {
+    return name().replace(ANDROID, "android-");
+  }
 
-	public static DeviceTargetPlatform fromPlatformVersion(String text) {
-		if (text != null) {
-			for (DeviceTargetPlatform b : DeviceTargetPlatform.values()) {
-				if (b.name().equals(ANDROID + text) || b.name().equals(text)) {
-					return b;
-				}
-			}
-		}
-		return null;
-	}
+  public static DeviceTargetPlatform fromPlatformVersion(String text) {
+    if (text != null) {
+      for (DeviceTargetPlatform b : DeviceTargetPlatform.values()) {
+        if (b.name().equals(ANDROID + text) || b.name().equals(text)) {
+          return b;
+        }
+      }
+    }
+    return null;
+  }
 
-	public static DeviceTargetPlatform fromInt(String text) {
-		if (text != null) {
-			for (DeviceTargetPlatform b : DeviceTargetPlatform.values()) {
-				if (b.name().equals(ANDROID + text)) {
-					return b;
-				}
-			}
-		}
-		return null;
-	}
+  public static DeviceTargetPlatform fromInt(String text) {
+    if (text != null) {
+      for (DeviceTargetPlatform b : DeviceTargetPlatform.values()) {
+        if (b.name().equals(ANDROID + text)) {
+          return b;
+        }
+      }
+    }
+    return null;
+  }
 
-	/**
-	 * @return version number of OS displayed on the device.
-	 */
-	public String getVersionNumber() {
-		return versionNumber;
-	}
+  /**
+   * @return version number of OS displayed on the device.
+   */
+  public String getVersionNumber() {
+    return versionNumber;
+  }
 
-	/**
-	 * @return api number, used in #{SelendroidCapabilities#PLATFORM_VERSION}
-	 */
-	public String getApi() {
-		return api;
-	}
+  /**
+   * @return api number, used in #{SelendroidCapabilities#PLATFORM_VERSION}
+   */
+  public String getApi() {
+    return api;
+  }
 
-	public String formatedName() {
-		return this.name() + "(" + this.versionNumber + ")";
-	}
+  public String formatedName() {
+    return this.name() + "(" + this.versionNumber + ")";
+  }
 }
